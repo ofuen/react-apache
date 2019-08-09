f8c89a1aef9007ce944536849d65aace7c25eab9
oscar <oscar@o2webdev.com>
2019-08-08 20:33:03 -0400

    Updates

---
+++ precache-manifest.921a905127a9a81480e065e2b753b2e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9467735ec88d46e6a333d606519b02",
+    "url": "/react-apache/index.html"
+  },
+  {
+    "revision": "7b8ddfab34b3a54ad38d",
+    "url": "/react-apache/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0291b19302247f45067e",
+    "url": "/react-apache/static/js/2.c312246f.chunk.js"
+  },
+  {
+    "revision": "7b8ddfab34b3a54ad38d",
+    "url": "/react-apache/static/js/main.e56b7148.chunk.js"
+  },
+  {
+    "revision": "95d017c6347245042c46",
+    "url": "/react-apache/static/js/runtime~main.b7a16b2c.js"
+  }
+]);
